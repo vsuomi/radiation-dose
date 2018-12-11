dbb810257f63939b403a88e0e4aacdddf6e98dff
cimppact-turku\visa <visa.suomi@gmail.com>
2018-12-11 14:36:10 +0200

    Feature imputation and scaling based only on training data

--- radiation_dose_estimator_keras.py
+++ radiation_dose_estimator_keras.py
@@ -45,121 +45,182 @@ pd.options.display.float_format = '{:.1f}'.format
 
 #%% read data
 
-dataframe = pd.read_csv(r'C:\Users\visa\Documents\TYKS\Machine learning\Radiation dose\radiation-dose\radiation_dose_data.csv', sep = ',')
+df = pd.read_csv(r'C:\Users\visa\Documents\TYKS\Machine learning\Radiation dose\radiation-dose\radiation_dose_data.csv', sep = ',')
+df_orig = df.copy()
 
 #%% check for duplicates
 
-duplicates = any(dataframe.duplicated())
-
-#%% calculate correlation and standard deviation matrices
-
-std_mat, corr_mat, most_corr = analyse_correlation(dataframe, 10, 'Korjattu_DAP_GYcm2')
-
-#%% handle nan values
-
-# calculate nan percent for each label
-
-nan_percent = pd.DataFrame(dataframe.isnull().mean() * 100, columns = ['% of NaN'])
-
-# drop nan values
-
-#dataframe = dataframe.dropna()
-dataframe = dataframe.dropna(subset = ['paino'])
-#dataframe = dataframe.dropna(subset = ['pituus'])
-
-# fill in mutually exclusive categorical values
-
-sten_post = dataframe[['sten_post_0', 'sten_post_25', 'sten_post_60', 
-                       'sten_post_85', 'sten_post_100']].idxmax(axis = 1)
-sten_post = sten_post.fillna(sten_post.mode()[0])
-sten_post = pd.get_dummies(sten_post).astype(int)
-sten_post = sten_post[['sten_post_0', 'sten_post_25', 'sten_post_60', 
-                       'sten_post_85', 'sten_post_100']]
-
-sten_pre = dataframe[['sten_pre_100', 'sten_pre_85', 'sten_pre_60']].idxmax(axis = 1)
-sten_pre = sten_pre.fillna(sten_pre.mode()[0])
-sten_pre = pd.get_dummies(sten_pre).astype(int)
-sten_pre = sten_pre[['sten_pre_100', 'sten_pre_85', 'sten_pre_60']]
-
-AHA = dataframe[['AHA_a', 'AHA_b1', 'AHA_b2', 'AHA_c', 'AHA_cto']].idxmax(axis = 1)
-AHA = AHA.fillna(AHA.mode()[0])
-AHA = pd.get_dummies(AHA).astype(int)
-AHA = AHA[['AHA_a', 'AHA_b1', 'AHA_b2', 'AHA_c', 'AHA_cto']]
-
-# replace missing values
-
-dataframe['paino'] = dataframe['paino'].fillna(dataframe['paino'].mean())
-dataframe['pituus'] = dataframe['pituus'].fillna(dataframe['pituus'].mean())
-dataframe['ind_pci_in_stemi'] = dataframe['ind_pci_in_stemi'].fillna(dataframe['ind_pci_in_stemi'].mode()[0])
-dataframe['ind_flap_failure'] = dataframe['ind_flap_failure'].fillna(dataframe['ind_flap_failure'].mode()[0])
-dataframe['ind_nstemi'] = dataframe['ind_nstemi'].fillna(dataframe['ind_nstemi'].mode()[0])
-dataframe['ind_diag'] = dataframe['ind_diag'].fillna(dataframe['ind_diag'].mode()[0])
-dataframe['ind_uap'] = dataframe['ind_uap'].fillna(dataframe['ind_uap'].mode()[0])
-dataframe['ind_heart_failure'] = dataframe['ind_heart_failure'].fillna(dataframe['ind_heart_failure'].mode()[0])
-dataframe['ind_stemi_other'] = dataframe['ind_stemi_other'].fillna(dataframe['ind_stemi_other'].mode()[0])
-dataframe['ind_stable_ap'] = dataframe['ind_stable_ap'].fillna(dataframe['ind_stable_ap'].mode()[0])
-dataframe['ind_arrhythmia_settl'] = dataframe['ind_arrhythmia_settl'].fillna(dataframe['ind_arrhythmia_settl'].mode()[0])
-dataframe['suonia_2_tai_yli'] = dataframe['suonia_2_tai_yli'].fillna(dataframe['suonia_2_tai_yli'].mode()[0])
-dataframe['lm_unprotected'] = dataframe['lm_unprotected'].fillna(dataframe['lm_unprotected'].mode()[0])
-dataframe['im'] = dataframe['im'].fillna(dataframe['im'].mode()[0])
-dataframe['lada'] = dataframe['lada'].fillna(dataframe['lada'].mode()[0])
-dataframe['ladb'] = dataframe['ladb'].fillna(dataframe['ladb'].mode()[0])
-dataframe['ladc'] = dataframe['ladc'].fillna(dataframe['ladc'].mode()[0])
-dataframe['lcxa'] = dataframe['lcxa'].fillna(dataframe['lcxa'].mode()[0])
-dataframe['lcxb'] = dataframe['lcxb'].fillna(dataframe['lcxb'].mode()[0])
-dataframe['lcxc'] = dataframe['lcxc'].fillna(dataframe['lcxc'].mode()[0])
-dataframe['ld1'] = dataframe['ld1'].fillna(dataframe['ld1'].mode()[0])
-dataframe['ld2'] = dataframe['ld2'].fillna(dataframe['ld2'].mode()[0])
-dataframe['lita'] = dataframe['lita'].fillna(dataframe['lita'].mode()[0])
-dataframe['lm'] = dataframe['lm'].fillna(dataframe['lm'].mode()[0])
-dataframe['lom1'] = dataframe['lom1'].fillna(dataframe['lom1'].mode()[0])
-dataframe['lom2'] = dataframe['lom2'].fillna(dataframe['lom2'].mode()[0])
-dataframe['lpd'] = dataframe['lpd'].fillna(dataframe['lpd'].mode()[0])
-dataframe['lpl'] = dataframe['lpl'].fillna(dataframe['lpl'].mode()[0])
-dataframe['ram_rv'] = dataframe['ram_rv'].fillna(dataframe['ram_rv'].mode()[0])
-dataframe['rcaa'] = dataframe['rcaa'].fillna(dataframe['rcaa'].mode()[0])
-dataframe['rcab'] = dataframe['rcab'].fillna(dataframe['rcab'].mode()[0])
-dataframe['rcac'] = dataframe['rcac'].fillna(dataframe['rcac'].mode()[0])
-dataframe['rita'] = dataframe['rita'].fillna(dataframe['rita'].mode()[0])
-dataframe['rpd'] = dataframe['rpd'].fillna(dataframe['rpd'].mode()[0])
-dataframe['rpl'] = dataframe['rpl'].fillna(dataframe['rpl'].mode()[0])
-dataframe['vgrca_ag'] = dataframe['vgrca_ag'].fillna(dataframe['vgrca_ag'].mode()[0])
-dataframe['vglca1_ag'] = dataframe['vglca1_ag'].fillna(dataframe['vglca1_ag'].mode()[0])
-dataframe['vglca2_ag'] = dataframe['vglca2_ag'].fillna(dataframe['vglca2_ag'].mode()[0])
-dataframe['restenosis'] = dataframe['restenosis'].fillna(dataframe['restenosis'].mode()[0])
-dataframe['stent_dimension'] = dataframe['stent_dimension'].fillna(dataframe['stent_dimension'].mean())
-dataframe['ball_dimension'] = dataframe['ball_dimension'].fillna(dataframe['ball_dimension'].mean())
-dataframe['add_stent_1'] = dataframe['add_stent_1'].fillna(0)
-dataframe['add_stent_2_tai_yli'] = dataframe['add_stent_2_tai_yli'].fillna(0)
-dataframe[['sten_post_0', 'sten_post_25', 'sten_post_60', 'sten_post_85', 'sten_post_100']] = sten_post
-dataframe[['sten_pre_100', 'sten_pre_85', 'sten_pre_60']] = sten_pre
-dataframe[['AHA_a', 'AHA_b1', 'AHA_b2', 'AHA_c', 'AHA_cto']] = AHA
-
-# check for nan values
-
-dataframe.isnull().values.any()
+duplicates = any(df.duplicated())
 
 #%% create synthetic features
 
-#dataframe['BSA'] = 0.007184 * dataframe['paino'].pow(0.425) * dataframe['pituus'].pow(0.725)
+df['BSA'] = 0.007184 * df['paino'].pow(0.425) * df['pituus'].pow(0.725)
+
+#%% calculate nan percent for each label
+
+nan_percent = pd.DataFrame(df.isnull().mean() * 100, columns = ['% of NaN'])
+
+# drop nan values
+
+#df = df.dropna()
+#df = df.dropna(subset = ['paino'])
+#df = df.dropna(subset = ['AHA_cto'])
+#df = df.dropna(subset = ['Patient_sex'])
+#df = df.dropna(subset = ['FN2BA'])
+#df = df.dropna(subset = ['I20.81_I21.01_I21.11_or_I21.41'])
+#df = df.dropna(subset = ['add_stent_2_tai_yli'])
+#df = df.dropna(subset = ['n_tmp_3'])
+#df = df.dropna(subset = ['sten_post_100'])
+#df = df.dropna(subset = ['suonia_2_tai_yli'])
+#df = df.dropna(subset = ['pituus'])
+
+#%% randomise and divive data for cross-validation
+
+# split data
+
+split_ratio = 0.2
+training_set, holdout_set = train_test_split(df, test_size = split_ratio)
+validation_set, testing_set = train_test_split(holdout_set, test_size = 0.5)
+
+# obtain sizes
+
+n_training = training_set.shape[0]
+n_validation = validation_set.shape[0]
+n_testing = testing_set.shape[0]
+
+#%% calculate correlation and standard deviation matrices
+
+std_mat, corr_mat, most_corr = analyse_correlation(training_set, 11, 'Korjattu_DAP_GYcm2')
 
 #%% analyse individual feature correlations
 
-analyse_feature_correlation(dataframe, 'paino', 'Korjattu_DAP_GYcm2', False)
-analyse_feature_correlation(dataframe, 'AHA_cto', 'Korjattu_DAP_GYcm2', True)
-analyse_feature_correlation(dataframe, 'Patient_sex', 'Korjattu_DAP_GYcm2', True)
-analyse_feature_correlation(dataframe, 'FN2BA', 'Korjattu_DAP_GYcm2', True)
-analyse_feature_correlation(dataframe, 'I20.81_I21.01_I21.11_or_I21.41', 'Korjattu_DAP_GYcm2', True)
-analyse_feature_correlation(dataframe, 'add_stent_2_tai_yli', 'Korjattu_DAP_GYcm2', True)
-analyse_feature_correlation(dataframe, 'n_tmp_3', 'Korjattu_DAP_GYcm2', True)
-analyse_feature_correlation(dataframe, 'sten_post_100', 'Korjattu_DAP_GYcm2', True)
-analyse_feature_correlation(dataframe, 'suonia_2_tai_yli', 'Korjattu_DAP_GYcm2', True)
+analyse_feature_correlation(training_set, 'paino', 'Korjattu_DAP_GYcm2', False)
+analyse_feature_correlation(training_set, 'AHA_cto', 'Korjattu_DAP_GYcm2', True)
+analyse_feature_correlation(training_set, 'Patient_sex', 'Korjattu_DAP_GYcm2', True)
+analyse_feature_correlation(training_set, 'FN2BA', 'Korjattu_DAP_GYcm2', True)
+analyse_feature_correlation(training_set, 'I20.81_I21.01_I21.11_or_I21.41', 'Korjattu_DAP_GYcm2', True)
+analyse_feature_correlation(training_set, 'add_stent_2_tai_yli', 'Korjattu_DAP_GYcm2', True)
+analyse_feature_correlation(training_set, 'n_tmp_3', 'Korjattu_DAP_GYcm2', True)
+analyse_feature_correlation(training_set, 'sten_post_100', 'Korjattu_DAP_GYcm2', True)
+analyse_feature_correlation(training_set, 'suonia_2_tai_yli', 'Korjattu_DAP_GYcm2', True)
+analyse_feature_correlation(training_set, 'pituus', 'Korjattu_DAP_GYcm2', False)
+
+#%% analyse target
+
+analyse_statistics(training_set[['Korjattu_DAP_GYcm2']])
+
+#%% replace missing values in all datasets
+
+# create dictionary for impute values based only on training data
+
+impute_values = {'BSA': training_set['BSA'].mean(),
+                 'paino': training_set['paino'].mean(),
+                 'pituus': training_set['pituus'].mean(),
+                 'ind_pci_in_stemi': training_set['ind_pci_in_stemi'].mode()[0],
+                 'ind_flap_failure': training_set['ind_flap_failure'].mode()[0],
+                 'ind_nstemi': training_set['ind_nstemi'].mode()[0],
+                 'ind_diag': training_set['ind_diag'].mode()[0],
+                 'ind_uap': training_set['ind_uap'].mode()[0],
+                 'ind_heart_failure': training_set['ind_heart_failure'].mode()[0],
+                 'ind_stemi_other': training_set['ind_stemi_other'].mode()[0],
+                 'ind_stable_ap': training_set['ind_stable_ap'].mode()[0],
+                 'ind_arrhythmia_settl': training_set['ind_arrhythmia_settl'].mode()[0],
+                 'suonia_2_tai_yli': training_set['suonia_2_tai_yli'].mode()[0],
+                 'lm_unprotected': training_set['lm_unprotected'].mode()[0],
+                 'im': training_set['im'].mode()[0],
+                 'lada': training_set['lada'].mode()[0],
+                 'ladb': training_set['ladb'].mode()[0],
+                 'ladc': training_set['ladc'].mode()[0],
+                 'lcxa': training_set['lcxa'].mode()[0],
+                 'lcxb': training_set['lcxb'].mode()[0],
+                 'lcxc': training_set['lcxc'].mode()[0],
+                 'ld1': training_set['ld1'].mode()[0],
+                 'ld2': training_set['ld2'].mode()[0],
+                 'lita': training_set['lita'].mode()[0],
+                 'lm': training_set['lm'].mode()[0],
+                 'lom1': training_set['lom1'].mode()[0],
+                 'lom2': training_set['lom2'].mode()[0],
+                 'lpd': training_set['lpd'].mode()[0],
+                 'lpl': training_set['lpl'].mode()[0],
+                 'ram_rv': training_set['ram_rv'].mode()[0],
+                 'rcaa': training_set['rcaa'].mode()[0],
+                 'rcab': training_set['rcab'].mode()[0],
+                 'rcac': training_set['rcac'].mode()[0],
+                 'rita': training_set['rita'].mode()[0],
+                 'rpd': training_set['rpd'].mode()[0],
+                 'rpl': training_set['rpl'].mode()[0],
+                 'vgrca_ag': training_set['vgrca_ag'].mode()[0],
+                 'vglca1_ag': training_set['vglca1_ag'].mode()[0],
+                 'vglca2_ag': training_set['vglca2_ag'].mode()[0],
+                 'restenosis': training_set['restenosis'].mode()[0],
+                 'stent_dimension': training_set['stent_dimension'].mean(),
+                 'ball_dimension': training_set['ball_dimension'].mean(),
+                 'add_stent_1': 0,
+                 'add_stent_2_tai_yli': 0}
+
+# combine datasets for imputing
+
+df = training_set.append([validation_set, testing_set])
+
+# impute data
+
+for key, val in impute_values.items():
+    df[key] = df[key].fillna(val)
+
+#%% fill in mutually exclusive categorical values
+
+# obtain categorical impute values
+
+sten_post_training = training_set[['sten_post_0', 'sten_post_25', 'sten_post_60', 
+                                   'sten_post_85', 'sten_post_100']].idxmax(axis = 1)
+impute_values['sten_post'] = sten_post_training.mode()[0]
+
+sten_pre_training = training_set[['sten_pre_100', 'sten_pre_85', 
+                                  'sten_pre_60']].idxmax(axis = 1)
+impute_values['sten_pre'] = sten_pre_training.mode()[0]
+
+AHA_training = training_set[['AHA_a', 'AHA_b1', 'AHA_b2', 
+                             'AHA_c', 'AHA_cto']].idxmax(axis = 1)
+impute_values['AHA'] = AHA_training.mode()[0]
+
+# impute data
+
+sten_post = df[['sten_post_0', 'sten_post_25', 'sten_post_60', 
+                'sten_post_85', 'sten_post_100']].idxmax(axis = 1)
+sten_post = sten_post.fillna(impute_values['sten_post'])
+sten_post = pd.get_dummies(sten_post).astype(int)
+sten_post = sten_post[['sten_post_0', 'sten_post_25', 'sten_post_60', 
+                       'sten_post_85', 'sten_post_100']]
+df[['sten_post_0', 'sten_post_25', 'sten_post_60', 'sten_post_85', 
+    'sten_post_100']] = sten_post
+
+sten_pre = df[['sten_pre_100', 'sten_pre_85', 'sten_pre_60']].idxmax(axis = 1)
+sten_pre = sten_pre.fillna(impute_values['sten_pre'])
+sten_pre = pd.get_dummies(sten_pre).astype(int)
+sten_pre = sten_pre[['sten_pre_100', 'sten_pre_85', 'sten_pre_60']]
+df[['sten_pre_100', 'sten_pre_85', 'sten_pre_60']] = sten_pre
+
+AHA = df[['AHA_a', 'AHA_b1', 'AHA_b2', 'AHA_c', 'AHA_cto']].idxmax(axis = 1)
+AHA = AHA.fillna(impute_values['AHA'])
+AHA = pd.get_dummies(AHA).astype(int)
+AHA = AHA[['AHA_a', 'AHA_b1', 'AHA_b2', 'AHA_c', 'AHA_cto']]
+df[['AHA_a', 'AHA_b1', 'AHA_b2', 'AHA_c', 'AHA_cto']] = AHA
+
+#%% check for nan values
+
+df.isnull().values.any()
+
+#%% split impute data back to training, validation and testing
+
+training_set = df[:n_training]
+validation_set = df[n_training:n_training+n_validation]
+testing_set = df[-n_testing:]
 
 #%% define feature and target labels
 
 feature_labels = ['paino', 'AHA_cto', 'Patient_sex', 'FN2BA',
                   'I20.81_I21.01_I21.11_or_I21.41', 'add_stent_2_tai_yli',
-                  'n_tmp_3', 'sten_post_100', 'suonia_2_tai_yli',]
+                  'n_tmp_3', 'sten_post_100', 'suonia_2_tai_yli', 'pituus']
 
 #feature_labels = ['paino', 'pituus', 'Patient_sex', 'Age', 
 #                  'I20.81_I21.01_I21.11_or_I21.41', 'I35.0', 'FN1AC', 'FN2BA',
@@ -181,45 +242,6 @@ target_label = ['Korjattu_DAP_GYcm2']
 
 #%% extract features and targets
 
-features = dataframe[feature_labels]
-targets = dataframe[target_label]
-
-#%% analyse target
-
-analyse_statistics(targets)
-
-#%% scale features
-
-# z-score
-
-#scaled_features = pd.DataFrame(sp.stats.mstats.zscore(features),
-#                               columns = list(features), 
-#                               index = features.index, dtype = float)
-
-# log (for skewed data)
-
-scaled_features = np.log1p(features)
-
-# box cox (for skewed data)
-
-#scaled_features = sp.special.boxcox1p(features, 0.15)
-
-#%% log transform targets (for skewed data)
-
-scaled_targets = np.log1p(targets)
-
-#%% combine dataframes
-
-concat_dataframe = pd.concat([scaled_features, scaled_targets], axis = 1)
-
-#%% randomise and divive data for cross-validation
-
-split_ratio = 0.2
-training_set, holdout_set = train_test_split(concat_dataframe, test_size = split_ratio)
-validation_set, testing_set = train_test_split(holdout_set, test_size = 0.5)
-
-#%% define features and targets
-
 training_features = training_set[feature_labels]
 validation_features = validation_set[feature_labels]
 testing_features = testing_set[feature_labels]
@@ -228,17 +250,48 @@ training_targets = training_set[target_label]
 validation_targets = validation_set[target_label]
 testing_targets = testing_set[target_label]
 
+#%% scale features
+
+# z-score
+
+t_mean = training_features.mean()
+t_std = training_features.std()
+
+training_features = (training_features - t_mean) / t_std
+validation_features = (validation_features - t_mean) / t_std
+testing_features = (testing_features - t_mean) / t_std
+
+# log (for skewed data)
+
+#training_features = np.log1p(training_features)
+#validation_features = np.log1p(validation_features)
+#testing_features = np.log1p(testing_features)
+
+# box cox (for skewed data)
+
+#lmbda = 0.15
+#
+#training_features = sp.special.boxcox1p(training_features, lmbda)
+#validation_features = sp.special.boxcox1p(validation_features, lmbda)
+#testing_features = sp.special.boxcox1p(testing_features, lmbda)
+
+#%% log transform targets (for skewed data)
+
+training_targets = np.log1p(training_targets)
+validation_targets = np.log1p(validation_targets)
+testing_targets = np.log1p(testing_targets)
+
 #%% build and train neural network model
 
 # define parameters
 
 learning_rate = 0.001
-n_epochs = 100
+n_epochs = 150
 n_neurons = 64
 n_layers = 2
 batch_size = 5
 l1_reg = 0.0
-l2_reg = 0.001
+l2_reg = 0.01
 batch_norm = False
 dropout = None
 
@@ -317,6 +370,9 @@ validation_predictions_lin = np.exp(validation_predictions) - 1
 
 # plot training performance
 
+#f1 = plot_regression_performance(history, training_targets, training_predictions, 
+#                                 validation_targets, validation_predictions)
+
 f1 = plot_regression_performance(history, training_targets_lin, training_predictions_lin, 
                                  validation_targets_lin, validation_predictions_lin)
 
@@ -350,9 +406,14 @@ variables_to_save = {'learning_rate': learning_rate,
                      'timestr': timestr,
                      'history': history,
                      'model_dir': model_dir,
-                     'dataframe': dataframe,
-                     'concat_dataframe': concat_dataframe,
-                     'holdout_set': holdout_set,
+                     'df': df,
+                     'df_orig': df_orig,
+                     'impute_values': impute_values,
+                     'feature_labels': feature_labels,
+                     'target_label': target_label,
+                     'n_training': n_training,
+                     'n_validation': n_validation,
+                     'n_testing': n_testing,
                      'training_set': training_set,
                      'training_features': training_features,
                      'training_targets': training_targets,
@@ -361,12 +422,7 @@ variables_to_save = {'learning_rate': learning_rate,
                      'validation_targets': validation_targets,
                      'testing_set': testing_set,
                      'testing_features': testing_features,
-                     'testing_targets': testing_targets,
-                     'scaled_features': scaled_features,
-                     'features': features,
-                     'targets': targets,
-                     'feature_labels': feature_labels,
-                     'target_label': target_label}
+                     'testing_targets': testing_targets}
     
 save_load_variables(model_dir, variables_to_save, 'variables', 'save')
 
